9590ad83db6e04dbd625dd68bfe688c4a6acb61d
tailand2000 <okada1126job@gmail.com>
2024-10-06 22:28:07 +0900

    [fix] config.pyの修正

--- app/config.py
+++ app/config.py
@@ -5,10 +5,9 @@ import openai
 def load_config():
     load_dotenv()
     # lang_chainの設定
-    os.environ["LANGCHAIN_API_KEY"] = os.environ.get("LANGCHAIN_API_KEY")
-    os.environ["LANGCHAIN_TRACING_V2"] = os.environ.get("LANGCHAIN_TRACING_V2")
-    os.environ["LANGCHAIN_PROJECT"] = os.environ.get("LANGCHAIN_PROJECT")
+    os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+    os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+    os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
 
 def load_openai_api_key():
-    load_dotenv()
-    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
